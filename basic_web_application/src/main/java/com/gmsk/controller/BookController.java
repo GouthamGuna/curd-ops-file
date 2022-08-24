77c732d8ca69f571a8b75ee3840284f921fed543
GouthamGuna <cerpsoftwaresolutionsyt@gmail.com>
2022-08-24 10:23:37 +0530

    aoc 3

---
+++ basic_web_application/src/main/java/com/gmsk/controller/BookController.java
@@ -0,0 +1,11 @@
+package com.gmsk.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+	
+
+}
