edb9eacd06e2defeff9d7cb7602bce914c7af58b
Goutham <ggowthamsankar@gmail.com>
2023-11-03 18:14:22 +0530

    Update UserController.java

--- java-servelt-json/src/main/java/in/dev/gmsk/controller/UserController.java
+++ java-servelt-json/src/main/java/in/dev/gmsk/controller/UserController.java
@@ -1,4 +1,31 @@
 package in.dev.gmsk.controller;
 
-public class UserController {
+import com.google.gson.Gson;
+import in.dev.gmsk.service.UserService;
+import in.dev.gmsk.service.serviceImpl.UserServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/users"}, name = "UserController", description = "UserController return json.")
+public class UserController extends HttpServlet {
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Gson gson = new Gson();
+        String userJson =gson.toJson(userService.getAllUserList());
+
+        PrintWriter printWriter = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        printWriter.write(userJson);
+        printWriter.close();
+    }
 }
