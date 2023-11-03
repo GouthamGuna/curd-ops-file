d0041b5555c2a08b6897447ddce0a5292b9da678
Goutham <ggowthamsankar@gmail.com>
2023-11-03 18:14:39 +0530

    Update User.java

--- java-servelt-json/src/main/java/in/dev/gmsk/model/User.java
+++ java-servelt-json/src/main/java/in/dev/gmsk/model/User.java
@@ -1,5 +1,17 @@
 package in.dev.gmsk.model;
 
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+    private String mobileNo;
 }
