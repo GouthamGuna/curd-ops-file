b69cbf30a47cd54047107d09c4a2908cfe09bc99
Goutham <ggowthamsankar@gmail.com>
2023-11-03 18:15:50 +0530

    Update UserServiceImpl.java

--- java-servelt-json/src/main/java/in/dev/gmsk/service/serviceImpl/UserServiceImpl.java
+++ java-servelt-json/src/main/java/in/dev/gmsk/service/serviceImpl/UserServiceImpl.java
@@ -1,4 +1,22 @@
 package in.dev.gmsk.service.serviceImpl;
 
-public class UserServiceImpl {
+import in.dev.gmsk.model.User;
+import in.dev.gmsk.service.UserService;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public List<User> getAllUserList() {
+        return Arrays.asList(
+                new User(20231124, "Gowtham Sankar", "Gunasekaran",
+                        "gowtham@gmsk.in", "9876543210"),
+                new User(20231127, "Karan", "Gunasekaran",
+                        "karan@gmsk.in", "987654123"),
+                new User(20231128, "Saran", "k",
+                        "saran@gmsk.in", "987654123")
+        );
+    }
 }
