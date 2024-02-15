f2a10baf0c8c88b8e2a4e17e58a496650b66dd4e
GouthamGuna <ggowthamsankar@gmail.com>
2024-02-15 07:39:10 +0530

    initial commit

---
+++ blog/blog/src/main/webapp/views/js/login.js
@@ -0,0 +1,22 @@
+console.log("JS init...");
+
+document.getElementById("myAnchor").addEventListener("click", function (event) {
+  event.preventDefault();
+
+  let userName = document.getElementById("userName");
+  let result = document.getElementById("result");
+
+  axios({
+    method: "POST",
+    url: "http://localhost:9090/blog/login.do",
+    params: { userName: userName.value },
+  }).then(
+    (response) => {
+      console.log("response : ", response.data);
+      result.innerHTML = `<h1>${response.data}</h1>`;
+    },
+    (e) => {
+      console.error(e);
+    }
+  );
+});
